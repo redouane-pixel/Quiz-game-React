55948ea227c5ab301bfd7cd8e27c4bbaa98344ed
redouane ha <ilk91874@gmail.com>
2021-12-27 17:03:34 +0000

    route changes
    I make some changes just for the programe work well I change the react router version to 5 for I can use switch.
    the result it's the same :)

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
-  useRouteMatch,
-  useParams
+  Link
 } from "react-router-dom";
 import DownPage from './components/firstPart'
 import TypeOfGamePage from './components/typeOfGame'
@@ -13,43 +11,47 @@ import SportPages from './components/sportPage'
 import SciencesPage from './components/sciencesPage'
 import HistoryPage from './components/HistoryPage'
 import EditPage from './components/editPage';
-// import {QuestionsData} from './components/QeustionsDataSciences';
+import {QuestionsData} from './components/QeustionsDataSciences';
 import { QeustionsContext } from './context';
 import "./styles/App.scss";
 function App() {
   const [display, setDisplay] = useState("DownPage")
-  // const [qeustionsData, setQeustionsData] = useState(QuestionsData);
+  const [qeustionsData, setQeustionsData] = useState(QuestionsData);
   setTimeout(() => {
     setDisplay("SecondPage")
   }, 6500);
+  // if you want to use context you can ust use QeustionContext/ you can check this:https://reactjs.org/docs/context.html
 return (
-  // <QeustionsContext.Provider value={[qeustionsData, setQeustionsData]}>
-     <Router>
-    <Route path="/" exact>
+    <Router>
+    <div>
+      {/* i use react-router version 5, because I need to use switch */}
+      <Switch>
+      <Route path="/" exact>
       <div className="containerFirstpage">
-          {display === "DownPage" &&(
-            <DownPage/>
-          )}
-          {display === "SecondPage" &&(
-            <TypeOfGamePage/>
-          )}
-          </div>
-      </Route>
-       <Route path="/sport" exact>
-        <SportPages/>
-      </Route>
-      <Route path="/sciences" exact>
-        <SciencesPage/>
-      </Route>
-      <Route path="/history" exact>
-        <HistoryPage/>
-      </Route>
-      {/* i make this for work in edit page that the user can edit the qeustion but I don't complete it, and you need to use context */}
+        {display === "DownPage" &&(
+          <DownPage/>
+        )}
+        {display === "SecondPage" &&(
+          <TypeOfGamePage/>
+        )}
+        </div>
+    </Route>
+    <Route path="/sport" exact>
+      <SportPages/>
+    </Route>
+    <Route path="/sciences" exact>
+      <SciencesPage/>
+    </Route>
+    <Route path="/history" exact>
+      <HistoryPage/>
+    </Route>
+     {/* i make this for work in edit page that the user can edit the qeustion but I don't complete it, and you need to use context */}
       {/* <Route path="/editPage" exact>
         <EditPage/>
       </Route> */}
-    </Router>
-// </QeustionsContext.Provider>
+      </Switch>
+    </div>
+  </Router>
   );
 }
 
